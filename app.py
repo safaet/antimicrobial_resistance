72c97d953138b540323dff62909b6a5ea1d8a9ac
safaet <safaetjaman@gmail.com>
2024-02-15 15:26:24 +0600

    add cross validation result in csv

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+
 from fastapi import FastAPI, File, UploadFile, HTTPException
 from fastapi.responses import JSONResponse
 import pandas as pd
@@ -20,9 +21,26 @@ from sklearn.ensemble import ExtraTreesClassifier
 from sklearn.ensemble import AdaBoostClassifier
 from sklearn.ensemble import GradientBoostingClassifier
 from sklearn.ensemble import BaggingClassifier
+from sklearn.ensemble import RandomForestClassifier, VotingClassifier
+from sklearn.linear_model import LogisticRegression
+from xgboost import XGBClassifier
+from sklearn.neural_network import MLPClassifier
+from sklearn.model_selection import cross_val_score
 
 from sklearn.metrics import f1_score
 from sklearn.metrics import auc
+import sklearn.metrics
+
+from fastapi.middleware.cors import CORSMiddleware
+
+clf1 = LogisticRegression(random_state=42)
+clf2 = XGBClassifier(random_state=42)
+clf3 = SVC(probability=True, random_state=42)
+
+log_reg_model = LogisticRegression(max_iter=1000, random_state=42)
+gb_model = GradientBoostingClassifier(random_state=42)
+nn_model = MLPClassifier(hidden_layer_sizes=(64, 32), max_iter=1000, random_state=42)
+xgb_model = XGBClassifier(random_state=42)
 
 models = []
 models.append(('LogR', LogisticRegression()))
@@ -37,6 +55,21 @@ models.append(('ABC', AdaBoostClassifier()))
 models.append(('GBC', GradientBoostingClassifier()))
 models.append(('ETC', ExtraTreesClassifier()))
 models.append(('BC', BaggingClassifier()))
+
+models.append(('ENS-1', VotingClassifier(
+    estimators=[
+        ('log_reg', log_reg_model),
+        ('gb', gb_model),
+        ('nn', nn_model),
+        ('xgb', xgb_model)
+    ],
+    voting='soft'  # Use 'hard' for majority voting or 'soft' for weighted voting
+)))
+
+models.append(('ENS-2', VotingClassifier(estimators=[
+    ('lr', clf1), ('rf', clf2), ('svc', clf3)
+],voting='soft'
+)))
 
 # def train_model():
 #     # Assuming you have a pre-existing CSV file for training the model
@@ -68,47 +101,64 @@ async def predict_csv(file: UploadFile = File(...)):
         temp_file.close()
 
     training_data = pd.read_csv(temp_file_path)
-    X_train = training_data.iloc[:, 1:-1]
-    y_train = training_data.iloc[:, -1]
+    X = training_data.iloc[:, 1:-1]
+    y = training_data.iloc[:, -1]
+
+    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
 
 
-    mode = []
-    tr_precision=[]
-    tr_recall = []
-    tr_f1 = []
-    tr_accu = []
-    tr_cv = []
-
-    te_precision=[]
-    te_recall = []
-    te_f1 = []
-    te_accu = []
-    te_cv = []
-    
+    li = []
 
     for name, model in models:
         model = model.fit(X_train, y_train)
-        mode.append(name)
         
         Y_train_pred = model.predict(X_train)
         Tr_precision = precision_score(y_train, Y_train_pred, average='macro').round(3)
-        tr_precision.append(Tr_precision)
         
         Tr_recall = recall_score(y_train, Y_train_pred, average='macro').round(3)
-        tr_recall.append(Tr_recall)
+
         
         Tr_f1 = f1_score (y_train, Y_train_pred, average='macro').round(3)
-        tr_f1.append(Tr_f1)
         
         accuracy = model.score(X_train, y_train).round(3)
-        tr_accu.append(accuracy)
+        
 
-        # cv_score = cross_val_score(model, X,y, cv=6)
-        # mean_accuracy = sum(cv_score)/len(cv_score)
-        # mean_accuracy = round(mean_accuracy, 2)
-        # # tr_cv.append(mean_accuracy)
+        cv_score = cross_val_score(model, X,y, cv=6)
+        mean_accuracy = sum(cv_score)/len(cv_score)
+        mean_accuracy = round(mean_accuracy, 2)
+        # tr_cv.append(mean_accuracy)
 
-    dict = {'classifier': mode, 'tr_precision': tr_precision, 'tr_recall': tr_recall, 'tr_f1 ': tr_f1,'tr_accuracy': tr_accu, 'tr_cv': tr_cv,
-         'te_precision': te_precision, 'te_recall': te_recall, 'te_f1 ': te_f1, 'te_accuracy': te_accu, 'te_cv': te_cv}
 
-    return dict
+        Y_test_pred = model.predict(X_test)
+        report = sklearn.metrics.classification_report(y_test, Y_test_pred)
+        Te_precision = precision_score(y_test, Y_test_pred, average='macro').round(3)
+        
+        Te_recall = recall_score(y_test, Y_test_pred, average='macro').round(3)
+        
+        Te_f1 = f1_score (y_test, Y_test_pred, average='macro').round(3)
+        
+        accuracy = model.score(X_test, y_test).round(3)
+
+        cv_score = cross_val_score(model, X,y, cv=6)
+        mean_accuracy = sum(cv_score)/len(cv_score)
+        mean_accuracy = round(mean_accuracy, 2)
+
+
+        dic = {f'classifier': name, 'tr_precision': Tr_precision, 'tr_recall': Tr_recall, 'tr_f1': Tr_f1,'tr_accuracy': accuracy, 'tr_cv': mean_accuracy,
+         'te_precision': Te_precision, 'te_recall': Te_recall, 'te_f1': Te_f1, 'te_accuracy': accuracy, 'te_cv': mean_accuracy}
+        
+        li.append(dic)
+
+    # dict = {'classifier': mode, 'tr_precision': tr_precision, 'tr_recall': tr_recall, 'tr_f1 ': tr_f1,'tr_accuracy': tr_accu, 'tr_cv': tr_cv,
+    #       'te_precision': te_precision, 'te_recall': te_recall, 'te_f1 ': te_f1, 'te_accuracy': te_accu, 'te_cv': te_cv}
+
+    return li
+
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],  # Allow all origins
+    allow_credentials=True,
+    allow_methods=["*"],  # Allow all methods
+    allow_headers=["*"],  # Allow all headers
+)
